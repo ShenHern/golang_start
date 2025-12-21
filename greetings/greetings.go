78c5ad7eb71a964ec969c2c63cf19dc0665784e4
ShenHern <go.shenhern@hotmail.com>
2025-12-21 23:14:01 +0800

    greetings-hello

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// if no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// return a greeting that embeds a name in the string
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 
+	"log"
+
 	"github.com/ShenHern/golang_start/greetings"
 )
 
 func main() {
+	log.SetPrefix("greetings:")
+	log.SetFlags(0)
 	// get a greeting message and print it
-	message := greetings.Hello("Shen Hern")
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
